86bf6b56021740c5fb721ff3865a78e010739ef6
Jakee <100876907+Jakeelamb@users.noreply.github.com>
2024-07-26 16:55:55 -0600

    Update MPI-Job-Template.sh

--- Templates/MPI-Job-Template.sh
+++ Templates/MPI-Job-Template.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1                         # Number of requested nodes
 #SBATCH --time=0:01:00                    # Max wall time
-#SBATCH --qos=testing                     # Specify testing QOS
-#SBATCH --partition=shas-testing          # Specify Summit haswell nodes
+#SBATCH --partition=short-cpu          
 #SBATCH --ntasks=4                        # Number of tasks per job
 #SBATCH --job-name=parallel_hello         # Job submission name
 #SBATCH --output=GENERAL_MPI_JOB.%j.out   # Output file name with Job ID
 
 # Written by: Daniel Trahan
 # Date: 20 April 2018
-# Updated: 25 July 2018
+# Updated: 26 July 2024
 # Purpose: This script submits a general MPI program to the Slurm job scheduler.
 
 # purge all existing modules
